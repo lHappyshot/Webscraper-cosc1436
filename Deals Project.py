48ed02a0312158558ec4eacc1e3073f9540b8cfe
lHappyshot <70824829+lHappyshot@users.noreply.github.com>
2020-12-07 10:34:19 -0600

    Add files via upload
    edit 1 commit

--- Deals Project.py
+++ Deals Project.py
@@ -28,22 +28,21 @@ import re
 import requests
 from tabulate import tabulate
 import smtplib
-import http.client
 import mysql.connector
-import ftplib
 from bs4 import BeautifulSoup
 from twilio.rest import Client
+import pandas as austin
 
 
 #* ---------------------------------------------------------------------------- #
 #*                                Root Variables                                #
 #* ---------------------------------------------------------------------------- #
 
-time = str(datetime.now().strftime("%d-%m-%y")) # today's date
+time = str(datetime.now().strftime("%d-%m-%y %H.%M")) # today's date
 timeCount = datetime.now() # today's date with mins and secs as an object
 
 mainURL = 'https://dealsea.com/' # used to combine with individual deal links
-websiteSource = "https://dealsea.com/Laptops-Desktops-completeTablets/b?node=3001"
+websiteSource = "https://dealsea.com/Laptops-Desktops-completeTablets/b?node=3001" # website to be scraped
 
 #* ---------------------------------------------------------------------------- #
 #*                                  Credentials                                 #
@@ -52,7 +51,7 @@ websiteSource = "https://dealsea.com/Laptops-Desktops-completeTablets/b?node=300
 twilioCredKeyOpen = open('twilioKEY.txt', 'r')
 twilioCredKey = twilioCredKeyOpen.readline()
 twilioCredKeyOpen.close()
-databasePassOpen = open('splPASSWORD.txt', 'r')
+databasePassOpen = open('sqlPASSWORD.txt', 'r')
 databasePass = databasePassOpen.readline()
 databasePassOpen.close()
 emailPassOpen = open('emailPASSWORD.txt', 'r')
@@ -64,6 +63,9 @@ emailAddressOpen.close()
 twilioNumberOpen = open('twilioNUMBER.txt', 'r')
 twilioNumber = twilioNumberOpen.readline()
 twilioNumberOpen.close()
+receiverNumberOpen = open('receiverNUMBER.txt', 'r')
+receiverNumber = receiverNumberOpen.readline()
+receiverNumberOpen.close()
 twilioAuthenticateOpen = open('twilioAUTH.txt', 'r')
 twilioAuthenticate = twilioAuthenticateOpen.readline()
 twilioAuthenticateOpen.close()
@@ -103,6 +105,7 @@ def websiteData(pageNumber, websiteSource):
 
 def scrapeEverything(response, mainURL):
     print('Begining to parse website...')
+    soup = ''
     try:
         soup = BeautifulSoup(response.content, "html.parser") # text rich html decode
         print('Website successfully parsed.')
@@ -137,17 +140,20 @@ def scrapeEverything(response, mainURL):
             if mainVendor == 'shop All':
                 mainVendor = ''
 
+            # Get deal link that takes you to the deals page and post
+            dealLink = mainURL + d.a.get('href')
+
             # Get other vendor options for the deal
             vendorOptions = d.findAll(href=re.compile(r"\/j\/4\/\?pid\="))
-            vendorOptions_dict = {}
+            allVendorOptions = {}
             for v in vendorOptions:
                 mainVendor = str(v.find(text=True))
                 # prevent duplicate vendor links
                 if mainVendor != 'Here': # filters false vendor names
                     vendorLink = mainURL + str(v['href'])
-                    if mainVendor not in vendorOptions_dict:
-                        vendorOptions_dict[mainVendor] = []
-                    vendorOptions_dict[mainVendor].append(vendorLink)
+                    if mainVendor not in allVendorOptions:
+                        allVendorOptions[mainVendor] = []
+                    allVendorOptions[mainVendor].append(vendorLink)
                 elif mainVendor == 'Here': # skips false vendor names
                     pass
 
@@ -174,8 +180,8 @@ def scrapeEverything(response, mainURL):
                 dealDetails = 'No details about deal.'
 
             # Add information to the list
-            completeTable.append([title, price, vendorOptions_dict, dealDetails])
-            simpleTable.append([title, priced, mainVendor, vendorLink, dealDetails])
+            completeTable.append([title, price, dealLink, allVendorOptions, dealDetails])
+            simpleTable.append([title, priced, mainVendor, dealLink, dealDetails])
 
         print('Website scrape successful.')
         print()
@@ -190,9 +196,9 @@ def scrapeEverything(response, mainURL):
 #? to that file by using data as input for the function.
 
 def saveDataToFile(table, time):
-    print("Creating new .txt, please wait...")
-    dataFileName = time + ' deals Time!.txt'
-    headers = ["Title", "Price", "Vendors", "deal Details"]
+    print("Creating new .txt file...")
+    dataFileName = time + ' Deals time!.txt'
+    headers = ["Title", "Price", "dealLink", "Vendors", "deal Details"]
     # io. for py 2 compatibility
     with io.open(dataFileName, 'w', encoding='utf-8') as scraper_table:
         # write table .txt file
@@ -206,13 +212,13 @@ def saveDataToFile(table, time):
 #?  of that file.
 
 def openDataOfFile(table, time):
-    dataFileName = time + ' Deals Time!.txt'
+    dataFileName = time + ' Deals time!.txt'
 
     isExist = os.path.exists(dataFileName) # check if file exists
     try:
         if isExist == True: # if exists open and return it to use
             print("Existing .txt file found.")
-            myFile = open(dataFileName, 'r', encoding='utf8')
+            myFile = open(dataFileName, 'r', encoding='utf8') # encoder must be specified
             myFile.readlines()
             myFile.close()
             print("Existing .txt file opened.")
@@ -256,25 +262,39 @@ def tableToTextConverter(table, time):
 #?  Display Deals Function - A simple function that print the deals found to the
 #? terminal in a pretty and organized fashion for beautiful viewing.
 
-##todo - try striping list brackets of inner lists
+#// - try striping list brackets of inner lists
+
 
 def displayDealsInTableOutput(prettyTable):
-    print()
-    print(prettyTable)
-    print()
+    for i in prettyTable: # pull the inner table
+        for j in i: # pull category from inside the current inner table
+            print(j) # print current element from inside the inner list
 
 #* ---------------------------------------------------------------------------- #
 
 #? Send to Database Function - a list can be send to a database with a set category
 #? of columns using the pandas library.
 
-#! NOT USEABLE - CANNOT CONNECT TO DATABASE.
-def sendToDatabase(table):
-    numOfDeals = len(table)
-    columns = [title, price, vendorOptions_dict, dealDetails]
-    sqlReady = austin.DataFrame(table, columns=columns)
-    print(sqlReady)
-    # director = databaseConnector.cursor()
+#! NOT USEABLE - CANNOT SEND TO DATABASE.
+def sendToDatabase(table, connection):
+    try:
+        print('Sending data to database...')
+        dataCursor = connection.cursor()
+        dataCursor.execute('CREATE TABLE deals (title, price, dealLink, allVendorOptions, dealDetails)')
+        dataCursor.commit()
+
+        numOfDeals = len(table)
+        columns = ['title', 'price', 'dealLink', 'allVendorOptions', 'dealDetails']
+        sqlReady = austin.DataFrame(table, columns=columns)
+
+        print(sqlReady)
+        connection.close()
+        print('Data sent to database.')
+        print()
+    except:
+        print('Error: Data to database not send.')
+        print()
+
     # for i in table[0:numOfDeals]:
     #     sql = "INSERT INTO `Dealsea` (`title`, `link`, `content`, `vendor`) VALUES (%s, %s, %s, %s);"
     #     val = (i.getTitle(),i.getLink(),i.getContent(),i.getVendor())
@@ -287,16 +307,15 @@ def sendToDatabase(table):
 #? Connect Database Function - using the login credentials, this function will connect
 #? to a mysql based database.
 
-#! NOT USEABLE - CANNOT CONNECT TO DATABASE.
 def connectToDatabase(databaseKey):
     print('attempting to connect to database...')
     try:
         connection = mysql.connector.connect(
-            host = '127.0.0.1',
-            user = 'hapaeria_wp801 ',
+            host = 'localhost',
+            user = 'root',
             password = databaseKey,
-            database = 'hapaeria_wp801'
-        )
+            database = 'all_deals')
+
         print("Connection successful.")
         print()
         return connection
@@ -309,26 +328,37 @@ def connectToDatabase(databaseKey):
 #? Deals extractor Function - used to take the large all-in-one deals list and  
 #? take one deal set at a time for adequate deals display via text and email.
 
-def dealsExtractor(table):
-    for i in table: # take first deal info
-        info = [] # empty list and location of first deal
-        for h in i:
+def dealsExtractor(table, numberOfDeals):
+    num = 0
+    info = 0
+    dealsTable = [] # master deal list with all captured deals
+    while num < numberOfDeals:
+        tableInstance = table[num]
+        info = [] # empty list and location of current deal
+        for h in tableInstance:
             if h == dict: # take first vendor from dictionary of vendors
                 for key, value in h.items():
                     info.append(value) # add first vendor empty list
-                    print(h)
-            elif h == 'No Price.': # skip if price was not found
-                pass
+                    #// info.append('\n\n')
+            elif h == 'No Price.': 
+                pass # skip if price was not found
             else:
                 info.append(h) # all other info add to list
-        return info
+                #// info.append('\n\n')
+        dealsTable.append([info])
+        num += 1 # while loop control and next deal
+    else:
+        print('All deal counts have been extracted.')
+        print()
+        return dealsTable
 
 #* ---------------------------------------------------------------------------- #
 
 #? Text Message Function - using the twilio api, this function send a text message
 #? to a sender with deals.
 
-def textMessage(twilioCredKey, twilioNumber, twilioAuthenticate, table):
+def textMessage(twilioCredKey, twilioNumber, receiverNumber, twilioAuthenticate, table):
+    client = 0
     account_sid = twilioCredKey
     auth_token = twilioAuthenticate
 
@@ -341,12 +371,34 @@ def textMessage(twilioCredKey, twilioNumber, twilioAuthenticate, table):
         client = Client(account_sid, auth_token) # connect to twilio rest.API service
     except:
         print('connection to text messaging service failed.')
+
+    try:
+    #! CODE NOT AVAILABLE TO TWILIO TRIAL ACCOUNTS. PLEASE UPGRADE TO PAID ACCOUNT TO USE.
+        print('checking if phone number is verified...')
+        toPhoneNumber = str(validatedNumber('''Please enter receiver of the text message.
+Input a 10 digit phone number(no spaces or dashes): ''')) # get phone number from user
+        print()
+
+        outgoing_caller_ids = client.outgoing_caller_ids.list(limit=20) # list of verified numbers
+
+        if toPhoneNumber not in outgoing_caller_ids: # cross check inputted number with verified list
+            validation_request = client.validation_requests \
+                .create( # if not present, add phone number to verified list after validation
+            friendly_name=validatedString('Please enter what to call this new number: '),
+                phone_number='+1' + str(toPhoneNumber))
+
+            print(validation_request.friendly_name) # validation code
+    except:
+        print('Error: Twilio account is in trial.')
+        print('Please upgrade to utilize validation of phone number with rest API.')
+        print()
+
     try:
         print('sending message...')
         message = client.messages.create( # send message command with details
-            to = '+18326871469',
+            to = '+1' + str(receiverNumber),
             from_ = str(twilioNumber),
-            body = singleDealTitle + '\n' + singleDealLink)
+            body = singleDealTitle + '\n' + '\n' + singleDealLink)
 
         print("message send.") # display body of the message
         print()
@@ -364,11 +416,29 @@ def validatedNumber(statement):
             rawNumber = int(input(statement))
         except ValueError:
             print('Response is not a number only. try again.')
+            print()
             continue
         else:
             break
 
     return rawNumber
+
+#* ---------------------------------------------------------------------------- #
+
+#? Validated String Function - validates and makes sure that the string is useable.
+
+def validatedString(statement):
+    while True:
+        try:
+            rawString = str(input(statement))
+        except ValueError:
+            print('Response is not valid. try again.')
+            print()
+            continue
+        else:
+            break
+
+    return rawString
 
 #* ---------------------------------------------------------------------------- #
 
@@ -416,35 +486,46 @@ def elapseTime(time):
 #? information of the current deals by email with the chosen server and 
 #? corrent email/ password credentials to send from.
 
-#! NOT USEABLE - CANNOT SEND EMAIL WITH SENDMAIL. SYNTAX FORMAT ERROR.
 def emailMessage(emailAddress, emailKey, table):
+    print('Starting email service...')
+    numOfDeals = validatedNumber("please enter number of deals to email: ")
+    extractedDeal = dealsExtractor(table, numOfDeals)
 
-    singleDeal = dealsExtractor(table)
+    #// dealNameCount = str(1)
+    #// while True:
+    #//     for i in extractedDeal:
+    #//         title  = i[0]
+    #//         price = i[1]
+    #//         dealLink = i[2]
+    #//        vendor = i[3]
+    #//         details = i[4]
+    #//         'deal' + dealNameCount = title + price + dealLink + vendor + details
+    #//        dealNameCount += 1
 
     sendFrom = 'hapcoscdemo@gmail.com'
-    sendTo = 'hapcoscdemo@gmail.com'
+    sendTo = str(validatedStatement('Please enter receiver of the email: '))
 
     subject = "Subject: Best deals of the day"
-    body = singleDeal
+    body = extractedDeal
 
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login(emailAddress, emailKey)
 
-    # try:
-    print('Connecting to e-mail server...')
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
-        smtp.login(emailAddress, emailKey)
-        subject = "Subject: Best deals of the day"
-        body = singleDeal
-        msg = f'Subject: {subject}\n\n{body}'
-        smtp.sendmail(sendFrom, sendTo, msg)
-        print('Connection to e-mail server successful.')
+            subject = "Subject: Best deals of the day"
+            body = str(extractedDeal)
+            msg = f'Subject: {subject}\n\n{body}'
+
+            smtp.sendmail(sendFrom, sendTo, msg)
+
+            print('Email message send.')
+            print()
+    except:
+        print('error: Connection to e-mail server failed or unable to send email.')
         print()
-    # except:
-    #     print('Connection to e-mail server failed.')
-    # except:
-    #     print ("Error: unable to send email.")
 
 #? ---------------------------------------------------------------------------- #
 #?                                 Main Program                                 #
@@ -456,8 +537,10 @@ webData = websiteData(1, websiteSource)
 table, simTable = scrapeEverything(webData, mainURL)
 statusUpdate1(table)
 prettyTextTable = openDataOfFile(table, time)
-emailMessage(emailAddress, emailPass, table)
-textMessage(twilioCredKey, twilioNumber, twilioAuthenticate, simTable[0])
+dataBaseObject = connectToDatabase(databasePass)
+sendToDatabase(table, dataBaseObject)
+emailMessage(emailAddress, emailPass, simTable)
+textMessage(twilioCredKey, twilioNumber, receiverNumber, twilioAuthenticate, simTable[0])
 end = elapseTime(timeCount)
-print('program complete. Thank you for using our WebScraper.')
+print('program complete. Thank you for using our Webscraper.')
 print()
